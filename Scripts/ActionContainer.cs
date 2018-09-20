d0b0edebf05ad0bd240314838a661aea87af7b68
ScottAHart <43241553+ScottAHart@users.noreply.github.com>
2018-09-20 22:44:20 +0930

    Scripts folder

---
+++ Scripts/ActionContainer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// An object that can have actions applied to it. 
+/// Holds what actions and supplies actions functions
+/// </summary>
+public class ActionContainer : MonoBehaviour, IActions{
+
+    public List<Action> _actions;
+
+    public void Apply(Action act)
+    {
+        act.Apply(gameObject);
+    }
+
+    public List<Action> GetActions()
+    {
+        return _actions;
+    }
+}
---
+++ Scripts/PostProcess.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// Simple post process attached to camera to render post process shader
+/// </summary>
+public class PostProcess : MonoBehaviour {
+    public Material _materail;
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        Graphics.Blit(source, destination, _materail);
+        
+    }
+}
